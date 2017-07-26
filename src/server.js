75d32dd06fdfec05ef54ac4f4710dc44ec0b7659
matte21 <matteoolivi7@gmail.com>
2017-07-26 22:46:54 +0200

    Roll back to previous version of server.js, that was correct

--- src/server.js
+++ src/server.js
@@ -40,9 +40,9 @@ var server = http.createServer( function(request, response) {
 	var filePath = '';
 
 	if (request.url === '/') {
-		filePath = '../public/index.html';
+		filePath = 'public/index.html';
 	} else {
-		filePath = '../public' + request.url;
+		filePath = 'public' + request.url;
 	}
 
 	var absPath = '../' + filePath;
